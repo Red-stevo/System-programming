cc15e996fff2a53f417857ddbf1f94f35f6a9323
Evans Mungai <69681950+EvansMungai@users.noreply.github.com>
2024-06-10 15:00:26 +0300

    Create sum computation of digits entered

--- Question 9/sum.c
+++ Question 9/sum.c
@@ -1,3 +1,20 @@
-// Prompt the user for a positive integer
+#include <stdio.h>
+
+int main() {
+    int num, sum = 0;
+
+    // Prompt the user for a positive integer
     printf("Enter a positive integer: ");
     scanf("%d", &num);
+
+
+
+    // Compute the sum of the digits
+    while (num > 0) {
+        sum += num % 10; // Add the last digit to the sum
+        num /= 10;       // Remove the last digit from the number
+    }
+
+
+    return 0;
+}
