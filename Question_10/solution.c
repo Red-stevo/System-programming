db7c2f610bdb15a82714733a6e4f2bf83ade73f0
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-06-12 05:20:20 +0300

    Commit message >>>>> finished on the boiler plate. <<<<<

--- Question_10/solution.c
+++ Question_10/solution.c
@@ -10,8 +10,53 @@ struct contact {
 int main(){
   //create the persons array  instance of the structure we deffined.
   struct contact usercontacts[12];
+  char choice;
 
-  //create the user manu.
-  printf("Select An Operation From The Menu.");
+  while(true){
+   //create the user manu.
+    printf("Select An Operation From The Menu.");
 
+    printf("\t1. Add a new Contact.\n");
+    printf("\t2. Update a Contact.\n")
+    printf("\t3. View Contacts.\n")
+    printf("\t4. Delect a Conatct.\n")
+    printf("\tQ. Quit.\n")
+
+    printf("Choice : ")
+    scanf("%c", &choice);
+
+    
+    switch(choice){
+      case '1':
+        //handle new contact.
+        break;
+      case '2':
+        //handle contacts update.
+        break;
+      case '3':
+        //view contacts.
+        break;
+      case '4':
+        //delete a contact.
+        break;
+      case 'Q':
+        //quit the application.
+        printf("Thank You For Using Our Sevices.");
+        exit(0);
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+  }
 }
