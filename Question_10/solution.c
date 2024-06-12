f7c569af75acb87b8b110572535086dad25f0d19
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-06-12 05:01:17 +0300

    Commit message >>>>> cretae the array to hold the user contact information <<<<<

--- Question_10/solution.c
+++ Question_10/solution.c
@@ -5,3 +5,13 @@ struct contact {
   char secondname[21];
   char phonenumber[14];
 };
+
+
+int main(){
+  //create the persons array  instance of the structure we deffined.
+  struct contact usercontacts[12];
+
+  //create the user manu.
+  printf("Select An Operation From The Menu.");
+
+}
